d888e0844f6c782baf534c9f79081f9ea7cfbea7
wendy <wangyn99@outlook.com>
2014-12-06 20:21:54 +0800

    controller

--- route_controllers/route_controllers.js
+++ route_controllers/route_controllers.js
@@ -1,22 +1,15 @@
-Router.route('/', {
-  name: 'home'
-});
 
-Router.route('/posts', {
-  controller: 'PostController',
-  action: 'index'
-});
-
-Router.route('/posts/:_id',{
-  controller: 'PostController',
-  action: 'show'
-});
 
 if(Meteor.isClient){
-  ApplicaiotnController = RouteController.extend({
-    layoutTemplate: 'appLayout',
+  
+  //create a custom RouteController "ApplicaiotnController" like this: 
+  //ApplicaiotnController = RouteController.extend();
+  
+  ApplicationController = RouteController.extend({
+    layoutTemplate: 'AppLayout',
     
     onBeforeAction: function(){
+      //do some logic checks or other custom logic
       console.log('app before hook!');
       this.next();
     },
@@ -27,19 +20,37 @@ if(Meteor.isClient){
   
   //inherit from other RouteControllers to model your application's behavior
   //you can separate your route logic into RouteController files instead of putting all of your complex business logic into one big route file.
-  HomeController = ApplicaiotnController.extend({
+  
+  HomeController = ApplicationController.extend({
     action: function(){
       //'this' is an instance of a 'RouteController'
-      this.render('home');
+      this.render('Home');
     }
   });
   
-  PostController = ApplicaiotnController.extend({
+  //inherit from 'ApplicaiotnController' and override any behavior you'd like.
+  PostController = ApplicationController.extend({
     show: function(){
-      this.render('postShow');
+      this.render('PostShow');
     },
     index: function(){
-      this.render('postIndex');
+      this.render('PostIndex');
     }
   });
-}
+  
+}Router.route('/', {
+  name: 'Home'
+});
+
+Router.route('/posts', {
+  //tell the route to use which controller
+  controller: 'PostController',
+  action: 'index'
+});
+
+Router.route('/posts/:_id',{
+  controller: 'PostController',
+  action: 'show'
+});
+
+
